90d3c5ff9531d3927ab141d0f7241849ba101ba3
Veronika Savluk <s_veronica@ukr.net>
2022-08-21 02:28:10 +0300

    distribution of fns by files

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
-import axios from 'axios';
 import Notiflix from 'notiflix';
-import SimpleLightbox from "simplelightbox";
 
-import "simplelightbox/dist/simple-lightbox.min.css";
-
-import RequestAPI from './js/fetchcreate';
+import { RequestAPI, PER_PAGE } from './js/fetchcreate';
 import { photoGallery, renderPhotoGallery } from './js/template';
+import { reloadBtn } from './js/reloadbtn';
+import { onWindowsScroll } from './js/windowscroll';
 
-const requestAPI = new RequestAPI();
+export const requestAPI = new RequestAPI();
 const formEl = document.querySelector("#search-form");
-const reloadBtn = document.querySelector(".load-more");
-reloadBtn.style.display = "none";
 
 formEl.addEventListener("submit", onSearchBtnSubmit);
-reloadBtn.addEventListener("click", onreloadPageBtn);
-
 
 function onSearchBtnSubmit(e) {
   e.preventDefault();
@@ -22,28 +16,20 @@ function onSearchBtnSubmit(e) {
 
   requestAPI.queryURL = e.target.elements.searchQuery.value.trim().split(" ").join("+");
   requestAPI.resetPage();
-  requestAPI.fetchArticle().then(data => {
-    if (data.hits.length > 0 && requestAPI.queryURL !== "") {
-      Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`);
-
-      renderPhotoGallery(data.hits);
+  requestAPI.fetchArticle().then(({ totalHits, hits }) => {
+    if (totalHits > PER_PAGE) {
       reloadBtn.style.display = "block";
+    };
+    if (hits.length >= 1 && requestAPI.queryURL !== "") {
+      Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
+      renderPhotoGallery(hits);
       }
-      else {
+    else {
         Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
       }
-  });
+  }).catch(error => console.log(error));
 }
 
-function onreloadPageBtn(e) {
-  requestAPI.fetchArticle().then(data => {
-    renderPhotoGallery(data.hits);
-    }).catch(error => {
-      Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
-      reloadBtn.style.display = "none";
-    });
-
-}
 
 // // const searchBtn = document.querySelector("#search-form button");
 // // console.log(searchBtn);

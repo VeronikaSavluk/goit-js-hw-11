dddde79fe8ab1fabfd405da67edab7f5d6a954e5
Veronika Savluk <s_veronica@ukr.net>
2022-08-23 22:05:52 +0300

    add infinite scroll

--- src/js/windowscroll.js
+++ src/js/windowscroll.js
@@ -1,10 +1,29 @@
-import { photoGallery } from "./template";
+import Notiflix from 'notiflix';
 
-export function onWindowsScroll() {
-  let { height: cardHeight } = photoGallery.firstElementChild.getBoundingClientRect();
+import { refs } from "./constants";
+import { requestAPI, fetchArticle } from '../index';
+// import { RequestAPI, fetchArticle } from './fetchcreate';
+import { renderPhotoGallery } from "./template";
+
+export function onWindowScroll(e) {
+  let { height: cardHeight } = refs.photoGallery.firstElementChild.getBoundingClientRect();
   window.scrollBy({
     top: cardHeight * 2,
     behavior: "smooth",
   });
+//  ======== only without reloadbutton==========
+  console.log(data.hits.length);
+    if(window.scrollY + window.innerHeight >=
+      refs.photoGallery.scrollHeight
+      // && data - scroll
+    ) {
+    requestAPI.fetchArticle().then(({ hits }) => {
+      if (hits.length < refs.PER_PAGE) {
+        Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+        refs.photoGallery.classList.add("scroll--stop");
+      };
+      renderPhotoGallery(hits);
+    });
+  };
 };
-window.addEventListener("scroll", onWindowsScroll);
+window.addEventListener("scroll", onWindowScroll);
